f0ef16c8f9dd9321b015309e8a35a6ae3729c9b8
roseli09 <cruz.roseli09@gmail.com>
2021-09-24 22:31:31 -0500

    second commit

---
+++ docs/precache-manifest.c2787fe498a82d497768438c93f6bf2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71db8d9bdec51ff0a14b821e296a387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e8858491df842cef5d2",
+    "url": "/static/css/main.0a79e77b.chunk.css"
+  },
+  {
+    "revision": "2aaae38fbcb0ad5c6be9",
+    "url": "/static/js/2.e1449281.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1449281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8858491df842cef5d2",
+    "url": "/static/js/main.61b6d517.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
